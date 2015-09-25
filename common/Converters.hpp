d472e8c96ff4ade83d6367167d6c0e081fe2ddab
shuda <lishuda1980@gmail.com>
2015-09-25 16:31:00 +0100

    Update

--- common/Converters.hpp
+++ common/Converters.hpp
@@ -1,34 +1,27 @@
-//Copyright(c) 2015 by Shuda Li[lishuda1980@gmail.com]
+//Copyright(c) 2015 Shuda Li[lishuda1980@gmail.com]
 //
-//Mapping and Relocalisation is licensed under the GPLv3 license.
-//Details can be found in the following.
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
 //
-//For using the code or comparing to it in your research, you are
-//expected to cite :
-//Li, Shuda, &Calway, (2015) Andrew.RGBD Relocalisation Using
-//Pairwise Geometry and Concise Key Point Sets.
-//In Intl.Conf.on Robotics and Automatiro(ICRA) 2015.
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
 //
-//Permission is hereby granted, free of charge, to any person
-//obtaining a copy of this software and associated documentation
-//files(the "Software"), to deal in the Software without
-//restriction, including without limitation the rights to use,
-//copy, modify, merge, publish, distribute, sublicense, and / or sell
-//copies of the Software, and to permit persons to whom the
-//Software is furnished to do so, subject to the following
-//conditions :
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//FOR A PARTICULAR PURPOSE AND NON - INFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
 //
-//The above copyright notice and this permission notice shall be
-//included in all copies or substantial portions of the Software.
-//
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-//OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-//HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-//OTHER DEALINGS IN THE SOFTWARE.
+//You agree to acknowledge use of the Software in any reports or publications of
+//results obtained with the Software and make reference to the following publication :
+//Li, Shuda, &Calway, Andrew(2015).RGBD Relocalisation Using Pairwise Geometry
+//and Concise Key Point Sets.In Intl Conf.Robotics and Automation.
 
 #ifndef BTL_CONVERTER_HEADER
 #define BTL_CONVERTER_HEADER
